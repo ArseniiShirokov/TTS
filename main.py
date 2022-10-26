d0366bcf3f5f3aa6219ac5944d3f92ad3f9e179a
Arsenii Shirokov <arsenii.shirokov@evocargo.com>
2022-10-27 01:40:00 +0400

    +

--- main.py
+++ main.py
@@ -15,18 +15,22 @@ def compute_totals(graph: Graph, start: Node) -> pd.DataFrame:
     # Init start node
     change_total(outputs, start, -start.total)
     # Simple Ford-Bellman
-    while True:
+    iter_cnt = 0
+    while iter_cnt < len(outputs.index):
         changes = False
         for index, row in outputs.iterrows():
             node = Node(exchange=row['exchange'], token=row["token"], total=row["total"])
             if node.total is not None:
                 for edge in graph.get_edges(node):
                     new_node = node.apply_transform(edge)
-                    if new_node.total < get_total(outputs, new_node.exchange, new_node.token):
+                    cur_total = get_total(outputs, new_node.exchange, new_node.token)
+                    if cur_total is None or new_node.total < cur_total:
                         changes = True
                         change_total(outputs, new_node, new_node.total)
         if not changes:
+            print("Everything is ok, neg cycle doesn't exist")
             break
+        iter_cnt += 1
     return outputs
 
 
