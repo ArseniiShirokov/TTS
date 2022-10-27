41b77c7a2c30615a5a51e806d7b2c6fe80f0efaf
Arsenii Shirokov <arsenii.shirokov@evocargo.com>
2022-10-27 23:37:26 +0400

    +

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ def get_total(df: pd.DataFrame, exchange: str, token: str) -> float:
     return df.loc[(df["exchange"] == exchange) & (df['token'] == token), ["total"]]
 
 
+def iterate_by_edges(outputs: pd.DataFrame, graph: Graph):
+    # Iterate by nodes
+    for index, row in outputs.iterrows():
+        node = Node(exchange=row['exchange'], token=row["token"], total=row["total"])
+        # Check reachability
+        if node.total is not None:
+            # Iterate by edges
+            for edge in graph.get_edges(node):
+                yield node, edge
+
+
 def compute_totals(graph: Graph, start: Node) -> pd.DataFrame:
     outputs = graph.get_all_nodes()
     # Init start node
@@ -18,15 +29,13 @@ def compute_totals(graph: Graph, start: Node) -> pd.DataFrame:
     iter_cnt = 0
     while iter_cnt < len(outputs.index):
         changes = False
-        for index, row in outputs.iterrows():
-            node = Node(exchange=row['exchange'], token=row["token"], total=row["total"])
-            if node.total is not None:
-                for edge in graph.get_edges(node):
-                    new_node = node.apply_transform(edge)
-                    cur_total = get_total(outputs, new_node.exchange, new_node.token)
-                    if cur_total is None or new_node.total < cur_total:
-                        changes = True
-                        change_total(outputs, new_node, new_node.total)
+        for node, edge in iterate_by_edges(outputs, graph):
+            new_node = node.apply_transform(edge)
+            cur_total = get_total(outputs, new_node.exchange, new_node.token)
+            # Update if better path was founded
+            if cur_total is None or new_node.total < cur_total:
+                changes = True
+                change_total(outputs, new_node, new_node.total)
         if not changes:
             print("Everything is ok, neg cycle doesn't exist")
             break
